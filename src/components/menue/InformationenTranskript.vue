4f3a062f4404e4772916c40e391e4a1ce5d0cee7
hcb79 <hcb@hcb.de>
2020-04-01 11:17:50 +0200

    Möglichkeit Token Reihung zu reparieren hinzugefügt. v0.44.0

--- src/components/menue/InformationenTranskript.vue
+++ src/components/menue/InformationenTranskript.vue
@@ -15,6 +15,7 @@
       <div><b>Tiers:</b>
         <span v-for="(aTier, aKey) in transcript.aTranskript.tiers" :key="'it-t' + aKey" :title="'ID: ' + aTier.pk">{{ (aKey > 0 ? ', ' : '') + aTier.tier_name }}</span>
       </div>
+      <button @click="resetTranskriptTokenReihung" v-if="resetTranskriptTokenReihungDone !== transcript.pk && (true || transcript && ((transcript.errors && transcript.errors.length > 0) || (transcript.aSVG && transcript.aSVG.errors && transcript.aSVG.errors.length > 0)))" :disabled="resetTranskriptTokenReihungLoading">Reihung aktuallisieren</button>
     </div>
   </div>
 </template>
@@ -25,10 +26,30 @@ export default {
   props: ['transcript'],
   data () {
     return {
-      show: true
+      show: true,
+      resetTranskriptTokenReihungLoading: false,
+      resetTranskriptTokenReihungDone: -1
     }
   },
   methods: {
+    resetTranskriptTokenReihung () {
+      console.log('resetTranskriptTokenReihung', this.transcript.pk)
+      this.resetTranskriptTokenReihungLoading = true
+      this.transcript.vueObj.$http.post('', {
+        getTranskript: this.transcript.pk,
+        resetTranskriptTokenReihung: 1
+      })
+        .then((response) => {
+          console.log('resetTranskriptTokenReihung', response.data)
+          this.resetTranskriptTokenReihungLoading = false
+          this.resetTranskriptTokenReihungDone = this.transcript.pk
+        })
+        .catch((err) => {
+          console.log(err)
+          alert('Fehler!')
+          this.resetTranskriptTokenReihungLoading = false
+        })
+    }
   },
   computed: {
     tokenCountByInf () {
